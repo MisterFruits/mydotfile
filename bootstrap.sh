85fd52fc60df89b41cc7c063da32e6e8fa6a7fdd
Victor <vic.toad.tor@free.fr>
2016-09-01 18:16:30 +0200

    NEW bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+# To be run from local folder !
+
+
+targets=(".aliases" ".bashrc" ".git-completion.bash" ".gitconfig" ".gitignore" ".profile" ".prompt" ".pythonstartup.py" "bin" ".vim" ".vimrc")
+for target in ${targets[*]}
+do
+    ln -bs -t $HOME $PWD/$target
+done
